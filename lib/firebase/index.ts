fec4a1bff2250ef2ba9a7fff8968685210e0abb6
Julian <jchumtl@gmail.com>
2022-04-13 03:58:56 -0400

    Setting up firebase and firestore

---
+++ lib/firebase/index.ts
@@ -0,0 +1,24 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getFirestore } from 'firebase/firestore';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAQnEEr8qKxyr1CcMCsGmiIyCwJ--Yh2Ic',
+//   authDomain: 'thepriceyapp.firebaseapp.com',
+//   projectId: 'thepriceyapp',
+//   storageBucket: 'thepriceyapp.appspot.com',
+//   messagingSenderId: '70370061229',
+//   appId: '1:70370061229:web:72e5e026262ed76ce10f24',
+//   measurementId: 'G-8QX4QM8YYW',
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const firestore = getFirestore(app);
+export {};
