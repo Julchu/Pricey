792ea32a6cdc6f52e49c2c4d9ad6bddb1d23414e
Julian <jchumtl@gmail.com>
2022-04-20 19:12:27 -0400

    Starting ReactHookForm, input designs

--- components/UI/Structure.tsx
+++ components/UI/Structure.tsx
@@ -8,4 +8,22 @@ export const Column = styled.div({
 export const Row = styled.div({
   display: 'flex',
   flexDirection: 'row',
+  marginBottom: '10px',
+});
+
+export const Grid = styled.div({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fill, 350px)',
+  columnGap: '44px',
+  rowGap: '44px',
+});
+
+export const Test = styled.div(() => ({
+  margin: '0px 10px',
+}));
+
+export const Line = styled.div({
+  margin: '20px -30px 50px -30px',
+  borderTop: '1px solid lightgrey',
+  boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
 });
