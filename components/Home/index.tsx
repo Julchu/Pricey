4ebf0c5aacfb030e8cb94c069ffebf7f5a965937
Julian <jchumtl@gmail.com>
2022-04-20 21:40:08 -0400

    Working basic input forms

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,13 +1,115 @@
 import { FC } from 'react';
+import { FormProvider, useForm, useFormContext } from 'react-hook-form';
 import { deleteUsers, getUsers, setUsers } from '../../lib/firebase';
-import { Hyperlink } from '../UI/Buttons';
-import { Row } from '../UI/Structure';
+import { Hyperlink, StripeButton } from '../UI/Buttons';
+import { Input } from '../UI/Form';
+import { Column, Grid, Line, Row } from '../UI/Structure';
 
-// Page shown at `localhost:3000/`
-const Index: FC = () => {
+// import { Timestamp } from 'firebase/firestore';
+
+type HomeProps = {
+  onSubmit: (data: SearchFormData) => Promise<void>;
+};
+
+export type SearchFormData = {
+  name: string;
+  price: number;
+  unit: string;
+  location?: string;
+
+  // Time that item was recorded
+  // time?: Timestamp;
+};
+
+const defaultFormValues = (): Partial<SearchFormData> => ({
+  // Add fields that are required for object submitted to Firebase as empty strings
+  name: '',
+  unit: '',
+  location: '',
+
+  // Time that item was recorded
+  // time?:
+});
+
+const IngredientForm: FC = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<SearchFormData>();
+
   return (
     <>
-      {/* <form> */}
+      <Row>
+        <Column style={{ width: '30%', marginRight: '20px' }}>
+          <Input {...register('name', { required: true })} placeholder="Search for an ingredient" />
+          {errors.name?.type === 'required' && 'Ingredient name is required'}
+        </Column>
+        <Column style={{ width: '10%', marginRight: '20px' }}>
+          <Input
+            {...register('price', { valueAsNumber: true, required: false })}
+            placeholder="Price"
+          />
+          {errors.price?.type === 'required' && 'Price is required'}
+        </Column>
+        <Column style={{ width: '10%', marginRight: '20px' }}>
+          <Input {...register('unit', { required: true })} placeholder="Unit" />
+          {errors.unit?.type === 'required' && 'Unit is required'}
+        </Column>
+        <StripeButton>Submit data</StripeButton>
+
+        {/* Controller for unit dropdown */}
+        {/* <Controller
+            name="firstName"
+            control={control}
+            render={({ field }) => <Input {...field} />}
+          />
+          <Controller
+            name="select"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={[
+                  { value: 'chocolate', label: 'Chocolate' },
+                  { value: 'strawberry', label: 'Strawberry' },
+                  { value: 'vanilla', label: 'Vanilla' },
+                ]}
+              />
+            )}
+          /> */}
+      </Row>
+    </>
+  );
+};
+
+// Page shown at `localhost:3000/`
+const Home: FC<HomeProps> = ({ onSubmit }) => {
+  const methods = useForm<SearchFormData>({ defaultValues: defaultFormValues() });
+  const { handleSubmit } = methods;
+
+  // Testing for result cards
+  const searchResults = ['a', 'b', 'c', 'd'];
+
+  return (
+    <>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <IngredientForm />
+          {/* <Column> */}
+          {/* </Column> */}
+        </form>
+      </FormProvider>
+
+      <Line />
+
+      <Row>
+        <Grid>
+          {searchResults.map((letter, index) => {
+            return <Card key={`${letter}_${index}`} letter={letter} />;
+          })}
+        </Grid>
+      </Row>
+
       <Row>
         <Hyperlink
           onClick={async () => {
@@ -37,9 +139,13 @@ const Index: FC = () => {
           deleteUsers
         </Hyperlink>
       </Row>
-      {/* </form> */}
     </>
   );
 };
 
-export default Index;
+// Search result cards
+const Card: FC<{ letter: string }> = ({ letter }) => {
+  return <>{letter}</>;
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Index from '../components/Home';
+import { useCallback } from 'react';
+import Home, { SearchFormData } from '../components/Home';
 import Layout from '../components/Layout';
 
 const IndexPage: NextPage = () => {
+  const onSubmit = useCallback(async (data: SearchFormData): Promise<void> => {
+    console.log(data);
+  }, []);
+
+  // const onSubmit = useCallback(
+  //   async (data: RecipeFormData, cb): Promise<void> => {
+  //     const ref = await updateRecipe(data);
+  //     cb ? cb(ref) : router.push('/recipes');
+  //   },
+  //   [updateRecipe, router],
+  // );
+
+  // const onDelete = useCallback(async (): Promise<void> => {
+  //   await remove();
+  //   router.push('/recipes');
+  // }, [remove, router]);
+
   return (
     <>
       <Head>
@@ -13,7 +31,7 @@ const IndexPage: NextPage = () => {
       </Head>
 
       <Layout>
-        <Index />
+        <Home onSubmit={onSubmit} />
       </Layout>
     </>
   );
