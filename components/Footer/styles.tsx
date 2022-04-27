344a81e37a1355fb82a78b1e5aa89305bdbe65df
Julian <jchumtl@gmail.com>
2022-04-27 02:02:20 -0400

    Fixing footer margin/padding

--- components/Footer/styles.tsx
+++ components/Footer/styles.tsx
@@ -1,1 +1,9 @@
-export {};
+import styled from '@emotion/styled';
+
+export const FooterInnerWrapper = styled.div({
+  width: '100%',
+  marginBottom: '30px',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fill, 250px)',
+  columnGap: '30px',
+});
