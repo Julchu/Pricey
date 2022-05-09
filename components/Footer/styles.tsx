fac08dcaae3fa1a2df9f33daa17e45d062a9a675
Julian <jchumtl@gmail.com>
2022-05-08 21:30:52 -0400

    Updating footer display and styling

--- components/Footer/styles.tsx
+++ components/Footer/styles.tsx
@@ -1,13 +1,16 @@
 import styled from '@emotion/styled';
 
 export const FooterInnerWrapper = styled.div(({ theme: { breakpoints } }) => ({
-  width: '100%',
-  marginBottom: '30px',
   display: 'grid',
   gridTemplateColumns: '1',
   columnGap: '30px',
   rowGap: '30px',
+
+  width: '100%',
+  marginBottom: '30px',
+
   [breakpoints.desktop]: {
-    gridTemplateColumns: 'repeat(auto-fill, 250px)',
+    gridTemplateColumns: 'auto auto auto auto',
+    // gridTemplateColumns: 'repeat(auto-fill, 250px)',
   },
 }));
