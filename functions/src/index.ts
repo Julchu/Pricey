237fe0def31600e0c404e90a7f8b7d610022c7a3
julchu <jchumtl@gmail.com>
2023-04-25 13:44:16 -0400

    Commenting out firebase-functions file for Vercel

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,12 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-// http://localhost:5001/anyprojectid/us-central1/helloWorld
-const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+// // http://localhost:5001/anyprojectid/us-central1/helloWorld
+// const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info('Hello logs!', { structuredData: true });
+//   response.send('Hello from Firebase!');
+// });
 
-export { helloWorld };
+// export { helloWorld };
+export {};
